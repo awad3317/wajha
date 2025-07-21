2b44acb6b3806edc9fd936b838b880599b6bd0b9
awad3317 <127610663+awad3317@users.noreply.github.com>
2025-07-21 17:12:42 +0300

    Update EstablishmentImageController.php

--- app/Http/Controllers/API/EstablishmentImageController.php
+++ app/Http/Controllers/API/EstablishmentImageController.php
@@ -5,13 +5,18 @@ namespace App\Http\Controllers\API;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Repositories\EstablishmentImageRepository;
+use Exception;
+use App\Services\ImageService;
+use Illuminate\Validation\Rule;
+use App\Classes\ApiResponseClass;
+use App\Repositories\EstablishmentRepository;
 
 class EstablishmentImageController extends Controller
 {
     /**
      * Create a new class instance.
      */
-    public function __construct(private EstablishmentImageRepository $EstablishmentImageRepository)
+    public function __construct(private EstablishmentImageRepository $EstablishmentImageRepository,private ImageService $imageService, private EstablishmentRepository $establishmentRepository)
     {
         //
     }
@@ -28,7 +33,26 @@ class EstablishmentImageController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        try {
+            $fields = $request->validate([
+                'establishment_id' =>['required',Rule::exists('establishments','id')],
+                'image' =>['required','image','max:2048'],
+            ]);
+            $establishment = $this->establishmentRepository->getById($fields['establishment_id']);
+        
+            if (auth('sanctum')->id() != $establishment->owner_id) {
+                return ApiResponseClass::sendError('Unauthorized: You are not the owner of this establishment.', [], 403);
+            }
+            $imagePath = $this->imageService->saveImage($fields['image'], 'establishment-image');
+            $EstablishmentImage=$this->EstablishmentImageRepository->store([
+                'establishment_id' => $fields['establishment_id'],
+                'image' => $imagePath,
+            ]);
+
+            return ApiResponseClass::sendResponse($EstablishmentImage,'Image saved successfully.');
+        } catch (Exception $e) {
+            return ApiResponseClass::sendError('Failed to saved image: ' . $e->getMessage());
+        }
     }
 
     /**
@@ -44,7 +68,29 @@ class EstablishmentImageController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+        try {
+            $fields = $request->validate([
+                'image' => ['required', 'image', 'max:2048'],
+            ]);
+
+            $establishmentImage = $this->EstablishmentImageRepository->getById($id);
+            if (!$establishmentImage) {
+                return ApiResponseClass::sendError('Establishment image not found', [], 404);
+            }
+
+            $establishment = $this->establishmentRepository->getById($establishmentImage->establishment_id);
+            if (auth('sanctum')->id() != $establishment->owner_id) {
+                return ApiResponseClass::sendError('Unauthorized: You are not the owner of this establishment.', [], 403);
+            }
+            $this->imageService->deleteImage($establishmentImage->image);
+            $imagePath = $this->imageService->saveImage($fields['image'], 'establishment-image');
+            $updated = $this->EstablishmentImageRepository->update([
+                'image' => $imagePath,
+            ], $id);
+            return ApiResponseClass::sendResponse($updated, 'Image updated successfully.');
+        } catch (Exception $e) {
+            return ApiResponseClass::sendError('Failed to update image: ' . $e->getMessage());
+        }
     }
 
     /**
@@ -52,6 +98,19 @@ class EstablishmentImageController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        try {
+            $establishmentImage = $this->EstablishmentImageRepository->getById($id);
+            $establishment = $this->establishmentRepository->getById($establishmentImage->establishment_id);
+            if (auth('sanctum')->id() != $establishment->owner_id) {
+                return ApiResponseClass::sendError('Unauthorized: You are not the owner of this establishment.', [], 403);
+            }
+            $this->imageService->deleteImage($establishmentImage->image);
+            if($this->EstablishmentImageRepository->delete($id)){
+                return ApiResponseClass::sendResponse([], 'Image deleted successfully.');
+            }
+            return ApiResponseClass::sendError('Establishment image not found', [], 404);
+        } catch (Exception $e) {
+            return ApiResponseClass::sendError('Failed to delete image: ' . $e->getMessage());
+        }
     }
 }
