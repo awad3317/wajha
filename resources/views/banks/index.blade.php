6757043a9a8aaa76a86a3fff00e9c41738b41892
awad3317 <awad3317@icloud.com>
2025-07-21 06:33:25 +0300

    update

--- resources/views/banks/index.blade.php
+++ resources/views/banks/index.blade.php
@@ -16,7 +16,7 @@
 @section('content')
 @livewire('banks')
 
-<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+<script src="{{ asset('vendor/sweetalert2/sweetalert2.all.min.js')}}"></script>
 <script>
     document.addEventListener('livewire:initialized', () => {
         Livewire.on('show-toast', ({ type, message }) => {
