7c0e3bae3124042bfffd26f2139c9f5c3e0a20d5
Mohammad_jaffer_alsaadi <Mohammadjaffrsaadix@gmail.com>
2025-08-29 21:44:09 +0500

    update

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,24 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import rtlcss from "rtlcss";
 
 export default defineConfig({
     plugins: [
         laravel({
             input: [
-                'resources/sass/app.scss',
+               "resources/css/adminlte.css",
+                "resources/css/adminlte-rtl.css",
                 'resources/js/app.js',
             ],
             refresh: true,
         }),
+           {
+            name: "rtlcss",
+            transform(code, id) {
+                if (id.includes("adminlte-rtl.css")) {
+                    return rtlcss.process(code);
+                }
+            },
+        },
     ],
 });
