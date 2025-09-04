d71ec5827edaf37b4c3cdfd5e29764ce680793e1
awad3317 <127610663+awad3317@users.noreply.github.com>
2025-09-04 12:01:17 +0300

    Update OwnerAccount.php

--- app/Models/OwnerAccount.php
+++ app/Models/OwnerAccount.php
@@ -27,6 +27,6 @@ class OwnerAccount extends Model
 
     public function bank()
     {
-        return $this->belongsTo(Bank::class);
+        return $this->belongsTo(bank::class, 'bank_id');
     }
 }
