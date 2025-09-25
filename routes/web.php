3073fd608cb91223de1a94d4c35ecd34878231d1
awad3317 <127610663+awad3317@users.noreply.github.com>
2025-09-25 09:33:45 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -24,20 +24,6 @@ use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
 
 // Route::get('/admin/login', App\Filament\Pages\CustomLogin::class);
 Route::resource('users', UserController::class);
-
-Route::post('/github/deploy', function (Request $request) {
-    $secret = '14171417Nn';
-
-    $signature = 'sha256=' . hash_hmac('sha256', $request->getContent(), $secret);
-    if (!hash_equals($signature, $request->header('X-Hub-Signature-256'))) {
-        Log::warning('Signature mismatch!');
-        abort(403, 'Unauthorized');
-    }
-
-    exec('cd /home/wajhah/htdocs/Wajhah-Project && git pull origin main');
-    return response()->json(['status' => 'Deployed']);
-});
-
 Auth::routes(['register' => false, 'reset' => false, 'verify' => false]);
 
 Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
