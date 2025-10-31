eb25c1800560913796a7233ef8e2947678c02737
Mohammad_jaffer_alsaadi <Mohammadjaffrsaadix@gmail.com>
2025-11-01 01:26:40 +0500

    Create hypersender.php

---
+++ config/hypersender.php
@@ -0,0 +1,8 @@
+<?php
+
+
+return [
+    'base_url' => env('HYPERSENDER_BASE_URL', 'https://app.hypersender.com'),
+    'instance_id' => env('HYPERSENDER_INSTANCE_ID'),
+    'token' => env('HYPERSENDER_TOKEN'),
+];
